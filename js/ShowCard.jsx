873165d2c67d4d580b18698d3a5736fbb52d1677
Susi Irwin <susi.irwin@gmail.com>
2018-03-26 08:27:31 -0600

    More work with testing react

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -24,6 +24,7 @@ const ShowCard = props => (
       <h3>{props.show.title}</h3>
       <h4>({props.show.year})</h4>
       <p>{props.show.description}</p>
+      <p>something different</p>
     </div>
   </Wrapper>
 );
