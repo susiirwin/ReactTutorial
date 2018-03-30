69f4a08235fdfe9c6667b148d2c97e8c72645eea
Susi Irwin <susi.irwin@gmail.com>
2018-03-30 08:42:37 -0600

    Completes display logic

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+// @flow
 
-const Details = props => (
-  <div className="details">
-    <pre>
-      <code>{JSON.stringify(props, null, 4)}</code>
-    </pre>
-  </div>
-);
+import React from 'react';
+import Header from './Header';
+
+const Details = (props: { show: Show }) => {
+  const { title, description, year, poster, trailer } = props.show;
+  return (
+    <div className="details">
+      <Header />
+      <section>
+        <h1>{title}</h1>
+        <h2>({year})</h2>
+        <img src={`/public/img/posters/${poster}`} alt={`Poster for ${title}`} />
+        <p>{description}</p>
+      </section>
+      <div>
+        <iframe
+          title={title}
+          src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0:amp;showinfo=0`}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Details;
--- js/Search.jsx
+++ js/Search.jsx
@@ -2,6 +2,7 @@
 
 import React, { Component } from 'react';
 import ShowCard from './ShowCard';
+import Header from './Header';
 
 class Search extends Component {
   state = {
@@ -10,7 +11,6 @@ class Search extends Component {
   props: {
     shows: Array<Show>
   };
-
   handleSearchTermChange = (event: SyntheticKeyboardEvent & { target: HTMLInputElement }) => {
     this.setState({ searchTerm: event.target.value });
   };
@@ -18,15 +18,7 @@ class Search extends Component {
   render() {
     return (
       <div className="search">
-        <header>
-          <h1>svideo</h1>
-          <input
-            onChange={this.handleSearchTermChange}
-            value={this.state.searchTerm}
-            type="text"
-            placeholder="Search"
-          />
-        </header>
+        <Header searchTerm={this.state.searchTerm} showSearch handleSearchTermChange={this.handleSearchTermChange} />
         <div>
           {this.props.shows
             .filter(
