5e7f9bd72447a55f1ef36051e6e9780a50d87c50
Susi Irwin <susi.irwin@gmail.com>
2018-04-07 14:59:20 -0600

    Adds more to redux

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,14 +1,23 @@
 // @flow
 
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setSearchTerm } from './actionCreators';
 
-const Landing = () => (
+const Landing = (props: { searchTerm: string, handleSearchTermChange: Function }) => (
   <div className="landing">
     <h1>svideo</h1>
-    <input type="text" placeholder="Search" />
+    <input onChange={props.handleSearchTermChange} value={props.searchTerm} type="text" placeholder="Search" />
     <Link to="/search">or Browse All</Link>
   </div>
 );
 
-export default Landing;
+const mapStateToProps = state => ({ searchTerm: state.searchTerm });
+const mapDispatchToProps = (dispatch: Fucntion) => ({
+  handleSearchTermChange(event) {
+    dispatch(setSearchTerm(event.target.value));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Landing);
